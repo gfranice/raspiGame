2a070b7be08a23bdb533936f27aeaf9b290038e7
gfranicevich <gfranicevich@mi.unc.edu.ar>
2025-04-18 20:59:51 -0300

    todavia no funciona con docker

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 @echo off
+tasklist /FI "IMAGENAME eq vcxsrv.exe" 2>NUL | find /I /N "vcxsrv.exe">NUL
+if "%ERRORLEVEL%" neq "0" (
+    REM modificar carpeta si es diferente
+    start "" "C:\Program Files\VcXsrv\vcxsrv.exe" :0 -multiwindow -ac -clipboard
+    timeout /t 2 /nobreak >nul
+)
+
 SET DISPLAY=host.docker.internal:0.0
 
 docker build -t raspberry-asm-dev .
